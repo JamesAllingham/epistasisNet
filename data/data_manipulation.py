fe48adb7434063d05e6a3c9fa5c4100cfbd70cdd
James Allingham <james.allingham@gmail.com>
2016-09-09 11:39:25 +0200

    the data manipulation file now infers the sizes of the input from the file being read meaning that the parameters for manipulating the data do not need to be changed for different data sets

--- data/data_manipulation.py
+++ data/data_manipulation.py
@@ -3,43 +3,44 @@ from random import sample, shuffle
 import math
 
 # Define some constants - perhaps these should be reworked so that they are input when running the script
-NUM_LOCI = 10 
-NUM_SAMPLES = 10000
-IN_FILE = 'simple_Models.txt_EDM-1/simple_Models.txt_EDM-1_1.txt'
+IN_FILE = 'small_EDM-1/small_EDM-1_1.txt'
 TEST_TRAIN_RATIO = 0.8
 
-# Read the data file and get the labels which are the last collumn
-labels = np.genfromtxt(IN_FILE, usecols=(100), dtype='intc', skip_header=1, max_rows=NUM_SAMPLES)
-# Read the data files and get the SNP information from the other collumns
-data = np.genfromtxt(IN_FILE, usecols=[90,91,92,93,94,95,96,97,98,99], dtype='intc', skip_header=1, max_rows=NUM_SAMPLES)
+# Read the data file, and get the numer of rows and collumns
+data = np.genfromtxt(IN_FILE, dtype='intc', skip_header=1)
+num_samples, num_loci = data.shape
+
+# Split into the inputs and outputs
+x = data[:,0:(num_loci-1)]
+y = data[:,num_loci-1]
 
 # We want the data to be in a 1-hot format indicating whether the SNP is double major, major-minor, or double minor
 # To do this we iterate through the data and for each element, we create a new 1-hot array
-data_1_hot = np.zeros((NUM_SAMPLES, NUM_LOCI, 3))
-for (i,row) in enumerate(data):
+x_1_hot = np.zeros((num_samples, num_loci, 3))
+for (i,row) in enumerate(x):
 	for (j,cell) in enumerate(row):
-		data_1_hot[i][j][0] = int(cell == 0)
-		data_1_hot[i][j][1] = int(cell == 1)
-		data_1_hot[i][j][2] = int(cell == 2)
+		x_1_hot[i][j][0] = int(cell == 0)
+		x_1_hot[i][j][1] = int(cell == 1)
+		x_1_hot[i][j][2] = int(cell == 2)
 
-#Labels need to also be 1-hot with [0]==no_epi, [1]==epi
-labels_1_hot = np.zeros((NUM_SAMPLES, 2))
-for (i,cell) in enumerate(labels):
-		labels_1_hot[i][0] = int(cell == 0)
-		labels_1_hot[i][1] = int(cell == 1)
+#Labels need to also be 1-hot with index 0 is control and index 1 is case
+y_1_hot = np.zeros((num_samples, 2))
+for (i,cell) in enumerate(y):
+		y_1_hot[i][0] = int(cell == 0)
+		y_1_hot[i][1] = int(cell == 1)
 
 # We now want to split the data into training and testing sets
 # We randomly choose a number of indices in the data that will be used for training
-training_indices = sample(range(NUM_SAMPLES),int(math.ceil(TEST_TRAIN_RATIO*NUM_SAMPLES)))
+training_indices = sample(range(num_samples),int(math.ceil(TEST_TRAIN_RATIO*num_samples)))
 shuffle(training_indices)
-train_x = data_1_hot[training_indices]
-train_y = labels_1_hot[training_indices]
+train_x = x_1_hot[training_indices]
+train_y = y_1_hot[training_indices]
 
 # All of the other indices are to become the testing set
-testing_indices = [elem for elem in range(NUM_SAMPLES) if elem not in training_indices]
+testing_indices = [elem for elem in range(num_samples) if elem not in training_indices]
 shuffle(testing_indices)
-test_x = data_1_hot[testing_indices]
-test_y = labels_1_hot[testing_indices]
+test_x = x_1_hot[testing_indices]
+test_y = y_1_hot[testing_indices]
 
 # Because we are sampling randomly, for large data sets, the ratio of case and controls in the data should remain 50% in both sets
 print "The number of training samples is %i with %i cases (%d percent)"%(len(train_y), sum(train_y[:,1]), np.mean(train_y[:,1])*100)
@@ -62,12 +63,12 @@ with open('test_y.npy','wb') as f:
 # We now want to split the data into training and testing sets
 # We randomly choose a number of indices in the data that will be used for training
 train_snp_data = data[training_indices]
-train_1_hot_labels = labels_1_hot[training_indices]
+train_1_hot_labels = y_1_hot[training_indices]
 
 # All of the other indices are to become the testing set
-testing_indices = [elem for elem in range(NUM_SAMPLES) if elem not in training_indices]
+testing_indices = [elem for elem in range(num_samples) if elem not in training_indices]
 test_snp_data = data[testing_indices]
-test_1_hot_labels = labels_1_hot[testing_indices]
+test_1_hot_labels = y_1_hot[testing_indices]
 
 
 # Because we are sampling randomly, for large data sets, the ratio of case and controls in the data should remain 50% in both sets
