4a74f053c154c5af259d3fb392d658f8b4e62774
James Allingham <james.allingham@gmail.com>
2016-09-11 16:04:42 +0200

    Imporved output messages

--- src/linear_model.py
+++ src/linear_model.py
@@ -3,7 +3,7 @@ import numpy as np
 import data_holder
 
 #load input data
-dh = data_holder.DataHolder('../data/small_EDM-1/small_EDM-1_1.txt')
+dh = data_holder.DataHolder('../data/medium_EDM-1/medium_EDM-1_1.txt', 0.2, 0.75)
 
 # get the data dimmensions
 num_rows_in, num_cols_in, num_states_in = dh.get_training_data().get_input_shape()
@@ -14,33 +14,36 @@ sess = tf.InteractiveSession()
 
 x = tf.placeholder(tf.float32, [None, num_cols_in, num_states_in])
 x_flat = tf.reshape(x, [-1, num_cols_in*num_states_in]) # flatten x
-W = tf.Variable(tf.zeros([num_cols_in*num_states_in,num_states_out]))
-b = tf.Variable(tf.zeros([num_states_out]))
-y = tf.nn.softmax(tf.matmul(x_flat, W) + b)
-
+W_1 = tf.Variable(tf.truncated_normal([num_cols_in*num_states_in,num_states_out], 0.1))
+b_1 = tf.Variable(tf.truncated_normal([num_states_out],0.1))
+y = tf.nn.softmax(tf.matmul(x_flat, W_1) + b_1)
 
 # Define loss and optimizer
 y_ = tf.placeholder(tf.float32, [None, num_states_out])
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.9).minimize(cross_entropy)  
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(cross_entropy)  
+
+# Define how to calculate accuracy
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # Train
 tf.initialize_all_variables().run()
 
-for i in range(2001):
-  batch_xs, batch_ys = dh.get_training_data().next_batch(50)
+for i in range(11):
+  batch_xs, batch_ys = dh.get_training_data().next_batch(100)
   train_step.run({x: batch_xs, y_: batch_ys})
-  if i % 100 == 0:
-    print('Batch %i complete' % (i + 1,))
+  test_accuracy = accuracy.eval({x: batch_xs, y_: batch_ys})
+  valid_xs, valid_ys = dh.get_validation_data().next_batch(100)
+  valid_accuracy = accuracy.eval({x: valid_xs, y_: valid_ys})
+  if (i % 1) == 0:
+    print('Batch %i complete, test accuracy = %f, validation accuracy = %f' % (i, test_accuracy, valid_accuracy))
+  if valid_accuracy + 0.1 < test_accuracy:
+    print('Stoping training with test accuracy = %f and validation accuracy = %f' % (test_accuracy, valid_accuracy))
+    break
 
 print('Training Complete')
 
 # Test trained model
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-test_xs, test_ys = dh.get_testing_data().next_batch(1000)
+test_xs, test_ys = dh.get_testing_data().next_batch(1200)
 print(accuracy.eval({x: test_xs, y_: test_ys}))
-
-# need to test different optimisers and different training rates
-# need to impliment validation to prevent over fitting
-# need to test different batch sizes a number of batches
