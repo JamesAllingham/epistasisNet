6ee8e2f3913730f12803293ccebf47fd107be38d
James Allingham <james.allingham@gmail.com>
2016-09-11 16:04:09 +0200

    fixed the data holder using the same data for all sets

--- src/data_holder.py
+++ src/data_holder.py
@@ -3,15 +3,15 @@ import data_loader
 
 class DataHolder:
     
-    def __init__(self, file_name_and_path, test_train_ratio=0.2, train_valid_ratio=0.95):
+    def __init__(self, file_name_and_path, test_train_ratio=0.2, train_valid_ratio=0.75):
         self.__data_loader = data_loader.DataLoader(file_name_and_path, test_train_ratio, train_valid_ratio)
         self.__data_loader.convert_data_to_1_hot()
         self.__data_loader.split_data()
         training_x, training_y = self.__data_loader.get_training_data()
         self.__training = data_batcher.DataBatcher(training_x, training_y)
-        testing_x, testing_y = self.__data_loader.get_training_data()
+        testing_x, testing_y = self.__data_loader.get_testing_data()
         self.__testing = data_batcher.DataBatcher(testing_x, testing_y)
-        validation_x, validation_y = self.__data_loader.get_training_data()
+        validation_x, validation_y = self.__data_loader.get_validation_data()
         self.__validation = data_batcher.DataBatcher(validation_x, validation_y)
 
     def get_testing_data(self):
