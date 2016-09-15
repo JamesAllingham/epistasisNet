8a66ff9fb43fdcc3328e8b0d8f39417c325ef573
James Allingham <james.allingham@gmail.com>
2016-09-15 22:52:54 +0200

    Added functionality for batching when there are two output data sets

--- src/data_batcher.py
+++ src/data_batcher.py
@@ -3,14 +3,19 @@ import numpy as np
 
 class DataBatcher:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y1, y2):
         self.__x = x
-        self.__y = y
+        self.__y1 = y1
+        self.__y2 = y2
         self.__batch_cursor = 0
         self.__data_size = self.__x.shape[0]
 
-        if self.__data_size != self.__y.shape[0]:
+        if self.__data_size != self.__y1.shape[0]:
             print("The input and output sets must have the same number of entries")
+            exit(2)
+
+        if self.__y2.shape[0] != self.__y1.shape[0]:
+            print("The output sets must have the same number of entries")
             exit(2)
 
     def next_batch(self, batch_size=50):
@@ -22,21 +27,27 @@ class DataBatcher:
             # If the batch size is less than the number of entries left in the data:
             # Take the next batch size number of elements and move the cursor forwards.
             x = self.__x[self.__batch_cursor:batch_size + self.__batch_cursor]
-            y = self.__y[self.__batch_cursor:batch_size + self.__batch_cursor]
+            y1 = self.__y1[self.__batch_cursor:batch_size + self.__batch_cursor]
+            y2 = self.__y2[self.__batch_cursor:batch_size + self.__batch_cursor]
             self.__batch_cursor = self.__batch_cursor + batch_size
         else:
             # If there is not enough data left then take the remaining data from the end and start again at the begining.
             x = self.__x[self.__batch_cursor:]
-            y = self.__y[self.__batch_cursor:]
+            y1 = self.__y1[self.__batch_cursor:]
+            y2 = self.__y2[self.__batch_cursor:]
             number_still_required = batch_size - (self.__data_size - self.__batch_cursor)
             x = np.concatenate((x, self.__x[0:number_still_required]))
-            y = np.concatenate((y, self.__y[0:number_still_required]))
+            y1 = np.concatenate((y1, self.__y1[0:number_still_required]))
+            y2 = np.concatenate((y2, self.__y2[0:number_still_required]))
             self.__batch_cursor = number_still_required
 
-        return (x, y)
+        return (x, y1, y2)
 
     def get_input_shape(self):
         return self.__x.shape
 
-    def get_output_shape(self):
-        return self.__y.shape
+    def get_output1_shape(self):
+        return self.__y1.shape
+
+    def get_output2_shape(self):
+        return self.__y2.shape
