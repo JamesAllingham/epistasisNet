1a070691ad65b73ae5193cb1ff9920a38ce07ef4
James Allingham <james.allingham@gmail.com>
2016-09-15 22:53:17 +0200

    Added support for creating batchers with two output data sets

--- src/data_holder.py
+++ src/data_holder.py
@@ -7,12 +7,12 @@ class DataHolder:
         self.__data_loader = data_loader.DataLoader(file_name_and_path, test_train_ratio, train_valid_ratio)
         self.__data_loader.convert_data_to_1_hot()
         self.__data_loader.split_data()
-        training_x, training_y = self.__data_loader.get_training_data()
-        self.__training = data_batcher.DataBatcher(training_x, training_y)
-        testing_x, testing_y = self.__data_loader.get_testing_data()
-        self.__testing = data_batcher.DataBatcher(testing_x, testing_y)
-        validation_x, validation_y = self.__data_loader.get_validation_data()
-        self.__validation = data_batcher.DataBatcher(validation_x, validation_y)
+        training_x, training_y1, training_y2 = self.__data_loader.get_training_data()
+        self.__training = data_batcher.DataBatcher(training_x, training_y1, training_y2)
+        testing_x, testing_y1, testing_y2 = self.__data_loader.get_testing_data()
+        self.__testing = data_batcher.DataBatcher(testing_x, testing_y1, testing_y2)
+        validation_x, validation_y1, validation_y2 = self.__data_loader.get_validation_data()
+        self.__validation = data_batcher.DataBatcher(validation_x, validation_y1, validation_y2)
 
     def get_testing_data(self):
         return self.__testing
