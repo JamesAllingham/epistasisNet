12fb55d26501ca047499901fc505e1ebc20c50df
James Allingham <james.allingham@gmail.com>
2016-09-20 09:38:03 +0200

    updated the non-linear model to use the utils script

--- src/nonlinear_model.py
+++ src/nonlinear_model.py
@@ -1,3 +1,5 @@
+
+from __future__ import absolute_import, division, print_function
 import tensorflow as tf
 import sys
 import getopt
@@ -5,8 +7,18 @@ import getopt
 import data_holder
 import utilities
 
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+flags.DEFINE_string('file_in', '', 'data in file location')
+flags.DEFINE_float('tt_ratio', 0.8, 'test:train ratio')
+flags.DEFINE_string('data_dir', '/tmp/logs/runx', 'Directory for storing data')
+flags.DEFINE_integer('max_steps', 10000, 'maximum steps ')
+flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate')
+flags.DEFINE_float('dropout', 0.9, 'Keep probability for training dropout')
+
 def train(file_name_and_path, test_train_ratio, log_file_path, max_steps, learning_rate, dropout_rate):
 
+  print(file_name_and_path)
   # Import data
   dh = data_holder.DataHolder(file_name_and_path, test_train_ratio, 1)
 
@@ -23,59 +35,25 @@ def train(file_name_and_path, test_train_ratio, log_file_path, max_steps, learni
     x = tf.placeholder(tf.float32, [None, num_cols_in, num_states_in], name='x-input')
     y1_ = tf.placeholder(tf.float32, [None, num_states_out1], name='y-input1')
     y2_ = tf.placeholder(tf.float32, [None, num_cols_out2, num_states_out2], name='y-input2')
-
-  def variable_summaries(var, name):
-    """Attach a lot of summaries to a Tensor."""
-    with tf.name_scope('summaries'):
-      mean = tf.reduce_mean(var)
-      tf.scalar_summary('mean/' + name, mean)
-      with tf.name_scope('stddev'):
-        stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
-      tf.scalar_summary('sttdev/' + name, stddev)
-      tf.scalar_summary('max/' + name, tf.reduce_max(var))
-      tf.scalar_summary('min/' + name, tf.reduce_min(var))
-      tf.histogram_summary(name, var)
-
-  def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
-    """Reusable code for making a simple neural net layer.
-    It does a matrix multiply, bias add, and then uses relu to nonlinearize.
-    It also sets up name scoping so that the resultant graph is easy to read,
-    and adds a number of summary ops.
-    """
-    # Adding a name scope ensures logical grouping of the layers in the graph.
-    with tf.name_scope(layer_name):
-      # This Variable will hold the state of the weights for the layer
-      with tf.name_scope('weights'):
-        weights = utilities.tn_weight_variable([input_dim, output_dim], 0.1)
-        variable_summaries(weights, layer_name + '/weights')
-      with tf.name_scope('biases'):
-        biases = utilities.bias_variable([output_dim])
-        variable_summaries(biases, layer_name + '/biases')
-      with tf.name_scope('Wx_plus_b'):
-        preactivate = tf.matmul(input_tensor, weights) + biases
-        tf.histogram_summary(layer_name + '/pre_activations', preactivate)
-      activations = act(preactivate, 'activation')
-      tf.histogram_summary(layer_name + '/activations', activations)
-      return activations
   
   x_flat = utilities.reshape(x, num_cols_in * num_states_in, 1)
 
-  hidden1 = nn_layer(x_flat, num_cols_in*num_states_in, 1000, 'hidden1')
-  hidden2 = nn_layer(hidden1, 1000, 500, 'hidden2')
+  hidden1 = utilities.nn_layer(x_flat, num_cols_in*num_states_in, 1000, 'hidden1')
+  hidden2 = utilities.nn_layer(hidden1, 1000, 500, 'hidden2')
 
   dropped, keep_prob = utilities.dropout(hidden2)
 
-  y1 = nn_layer(dropped, 500, num_states_out1, 'softmax_1', act=tf.nn.softmax)
-  y2 = utilities.reshape(nn_layer(dropped, 500, num_states_out2*num_cols_out2, 'softmax_2', act=tf.nn.softmax), num_cols_out2, num_states_out2)
+  y1 = utilities.nn_layer(dropped, 500, num_states_out1, 'softmax_1', act=tf.nn.softmax)
+  y2 = utilities.reshape(utilities.nn_layer(dropped, 500, num_states_out2*num_cols_out2, 'softmax_2', act=tf.nn.softmax), num_cols_out2, num_states_out2)
 
-  loss1 = utilities.calculate_cross_entropy(y1, y1_, '1')
-  loss2 = utilities.calculate_cross_entropy(y2, y2_, '2')
+  loss1 = utilities.calculate_cross_entropy(y1, y1_, name_suffix='1')
+  loss2 = utilities.calculate_cross_entropy(y2, y2_, name_suffix='2')
 
-  train_step1 = utilities.train(utilities.Optimizer.GradientDescent, learning_rate, loss1)
-  train_step2 = utilities.train(utilities.Optimizer.GradientDescent, learning_rate, loss2)
+  train_step1 = utilities.train(learning_rate, loss1, name_suffix='1')
+  train_step2 = utilities.train(learning_rate, loss2, name_suffix='2')
 
-  accuracy1 = utilities.calculate_accuracy(y1, y1_, '1')
-  accuracy2 = utilities.calculate_accuracy(y2, y2_, '2')
+  accuracy1 = utilities.calculate_accuracy(y1, y1_, name_suffix='1')
+  accuracy2 = utilities.calculate_accuracy(y2, y2_, name_suffix='2')
 
   # Merge all the summaries and write them out to /tmp/mnist_logs (by default)
   merged = tf.merge_all_summaries()
@@ -87,9 +65,9 @@ def train(file_name_and_path, test_train_ratio, log_file_path, max_steps, learni
   # Train the model, and also write summaries.
   # Every 10th step, measure test-set accuracy, and write test summaries
   # All other steps, run train_step on training data, & add training summaries
-
   def feed_dict(train):
-    """Make a TensorFlow feed_dict: maps data onto Tensor placeholders."""
+    """ Make a TensorFlow feed_dict: maps data onto Tensor placeholders. 
+    """
     if train:
       xs, y1s, y2s = dh.get_training_data().next_batch(100)
       k = dropout_rate
@@ -108,10 +86,8 @@ def train(file_name_and_path, test_train_ratio, log_file_path, max_steps, learni
       if i % 100 == 99:  # Record execution stats
         run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
         run_metadata = tf.RunMetadata()
-        summary, _, _ = sess.run([merged, train_step1, train_step2],
-                              feed_dict=feed_dict(True),
-                              options=run_options,
-                              run_metadata=run_metadata)
+        summary, _, _ = sess.run([merged, train_step1, train_step2], feed_dict=feed_dict(True),
+                              options=run_options, run_metadata=run_metadata)
         train_writer.add_run_metadata(run_metadata, 'step%03d' % i)
         train_writer.add_summary(summary, i)
         print('Adding run metadata for', i)
@@ -124,12 +100,13 @@ def train(file_name_and_path, test_train_ratio, log_file_path, max_steps, learni
 
 def main(args):
   # Try get user input   
-  input_file, test_train_ratio, log_file_path, max_steps, learning_rate, dropout_rate = utilities.get_command_line_input(args)
-
-  if tf.gfile.Exists(log_file_path):
-    tf.gfile.DeleteRecursively(log_file_path)
-  tf.gfile.MakeDirs(log_file_path)
-  train(input_file, test_train_ratio, log_file_path, max_steps, learning_rate, dropout_rate)
+  if not FLAGS.file_in:
+    print("Please specify the input file using the '--file_in=' flag.")
+    sys.exit(2)
+  if tf.gfile.Exists(FLAGS.data_dir):
+    tf.gfile.DeleteRecursively(FLAGS.data_dir)
+  tf.gfile.MakeDirs(FLAGS.data_dir)
+  train(FLAGS.file_in, FLAGS.tt_ratio, FLAGS.data_dir, FLAGS.max_steps, FLAGS.learning_rate, FLAGS.dropout)
 
 if __name__ == '__main__':
   tf.app.run()
